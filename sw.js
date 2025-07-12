c87bf93233df34eb9b62b6f576f1f3628b34c5e7
deelipshah7 <dilipshah.aus@gmail.com>
2025-07-12 14:26:57 +1000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'daily-shopping-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './icon-196.png',
+  './icon-512.png',
+  './manifest.webmanifest'
+];
+
+// Install event - cache all static assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch event - serve from cache, falling back to network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => {
+        if (event.request.mode === 'navigate') {
+          return caches.match('./');
+        }
+      })
+  );
+});
